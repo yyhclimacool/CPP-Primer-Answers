044ca2478d4f047653370ff4a35f6ede59527f85
yyhclimacool <yyhclimacool@gmail.com>
2016-05-19 18:24:11 +0800

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,1 +1,13 @@
+#include "Person.h"
 
+using namespace std;
+
+istream &read(istream & is, Person &p){
+    is >> name >> addr;
+    return is;
+}
+
+ostream &print(ostream &os, const Person &p){
+    os << p.name << p.addr;
+    return os;
+}
