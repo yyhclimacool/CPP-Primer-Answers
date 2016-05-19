b0ee8f38d417ac26c2ec629d3928aa7b443209fd
yyhclimacool <yyhclimacool@gmail.com>
2016-05-19 18:23:35 +0800

    Update Person.h

--- Person.h
+++ Person.h
@@ -6,12 +6,22 @@
 using namespace std;
 
 class Person{
+    friend istream &read(istream &, Person &);
+    friend ostream &print(ostream &, const Person &);
 public:
+    Person() = default;
+    Person(const string &n, const string &a):name(n), addr(a){}
+    
+    ~Person(){}
+    
     string getName() const { return name; }
     string getAddr() const { return addr; }
 private:
     string name;
     string addr;
 };
+
+istream &read(istream & is, Person &p);
+ostream &print(ostream &os, const Person &p);
 
 #enfif // _PERSON_H_
