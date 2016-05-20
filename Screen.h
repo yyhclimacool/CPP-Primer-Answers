892fd7fcb0c120ca18efa43858fc2707e65f61e2
yyhclimacool <yyhclimacool@gmail.com>
2016-05-20 17:02:28 +0800

    Exercise 7.27

--- Screen.h
+++ Screen.h
@@ -16,18 +16,45 @@ public:
     inline char get(pos ht, pos wd) const;
     
     Screen &move(pos ht, pos wd);
+
+    Screen &set(char c);
+    Screen &set(pos r, pos c, char ch);
+    Screen &display(ostream &os);
+    const Screen &display(ostream &os) const;
 private:
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
+
+    void do_display(ostream &os) const { os << contents;}
 };
 
-inline char get(pos r, pos c) const {
+inline char Screen::get(pos r, pos c) const {
     return contents[r * width + c];
 }
 
-inline Screen &move(pos r, pos c){
+inline Screen &Screen::move(pos r, pos c){
     cursor = r * width + c;
+    return *this;
+}
+
+inline Screen &Screen::set(char c){
+    contents[cursor] = ch;
+    return *this;
+}
+
+inline Screen &Screen::set(pos r, pos c, char ch){
+    contents[r * width + c] = ch;
+    return *this;
+}
+
+inline Screen &Screen::display(ostream &os){
+    do_display(os);
+    return *this;
+}
+
+inline const Screen &Screen::display(ostream &os) const {
+    do_display(os);
     return *this;
 }
 
