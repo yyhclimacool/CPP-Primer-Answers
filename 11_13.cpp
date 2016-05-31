892c092a371cbdc95b5b407af0d8e0637d1bb9e9
yyhclimacool <yyhclimacool@gmail.com>
2016-05-31 11:19:34 +0800

    Create 11_13.cpp
    The other way to create pair instance is comment out.

---
+++ 11_13.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+void vec_of_pair(vector<pair<string, int>> &pv, istream &is){
+	string word;
+	int ival;
+	while(is >> word >> ival){
+		pv.push_back(make_pair(word, ival));
+		// pv.push_back(pair<string, int>(word, ival));
+		// pv.push_back({word, ival});
+	}
+}
+
+int main(){
+	vector<pair<string, int>> myVec;
+	vec_of_pair(myVec, cin);
+	for(const auto & e : myVec){
+		cout << e.first << " " << e.second << endl;
+	}
+	return 0;
+}
