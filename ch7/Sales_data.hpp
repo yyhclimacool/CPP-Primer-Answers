3477dfa1584214ee0278430181f8f4dd2aa4f01a
yyhclimacool <yyhclimacool@gmail.com>
2016-11-09 04:52:09 -0500

    exercise 7.6

--- ch7/Sales_data.hpp
+++ ch7/Sales_data.hpp
@@ -16,14 +16,45 @@ class Sales_data{
 		string isbn() const { return bookNo;}
 		Sales_data &combine(const Sales_data &rhs);
 		double avg_price() const;
-	private:
+	// private:
 		string bookNo;
 		unsigned units_sold = 0;
 		double revenue = 0.0;
 };
 
+Sales_data &Sales_data::combine(const Sales_data &rhs){
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+
+double Sales_data::avg_price() const {
+	if(units_sold)
+		return revenue / units_sold;
+	else
+		return 0;
+}
+
 Sales_data add(const Sales_data &, const Sales_data &);
 ostream &print(ostream &, const Sales_data &);
-istream &input(istream &, Sales_data &);
+istream &read(istream &, Sales_data &);
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs){
+	Sales_data sum = lhs;
+	sum.combine(rhs);
+	return sum;
+}
+
+ostream &print(ostream &os, const Sales_data &rhs){
+	os << rhs.isbn() << " " << rhs.units_sold << " " << rhs.revenue << " " << rhs.avg_price;
+	return os;
+}
+
+istream &read(istream &is, Sales_data &rhs){
+	double price = 0.0;
+	is >> rhs.bookNo >> rhs.units_sold >> price;
+	rhs.revenue = rhs.units_sold * price;
+	return is;
+}
 
 #endif
