574c3a769012d14899f4e9d2f3f9bc3692041d2c
yyhclimacool <yyhclimacool@gmail.com>
2016-11-10 01:13:22 -0500

    exercise 7.21

--- ch7/Sales_data.hpp
+++ ch7/Sales_data.hpp
@@ -13,6 +13,9 @@ istream &read(istream &, Sales_data &);
 ostream &print(ostream &, const Sales_data &);
 
 class Sales_data{
+		friend Sales_data add(const Sales_data &, const Sales_data &);
+		friend istream &read(istream &, Sales_data &);
+		friend ostream &print(ostream &, const Sales_data &);
 	public:
 		Sales_data() = default;
 		Sales_data(const string &str):bookNo(str), units_sold(0), revenue(0.0){}
@@ -25,8 +28,9 @@ class Sales_data{
 		 */
 		string isbn() const { return bookNo;}
 		Sales_data &combine(const Sales_data &rhs);
+
+	private:
 		double avg_price() const;
-	// private:
 		string bookNo;
 		unsigned units_sold = 0;
 		double revenue = 0.0;
