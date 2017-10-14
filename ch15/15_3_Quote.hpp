d33a0702ae5a925d517045aa8b31e86f53539bc6
yyhclimacool <yyhclimacool@gmail.com>
2017-10-14 08:33:29 -0400

    chapter 15

---
+++ ch15/15_3_Quote.hpp
@@ -0,0 +1,24 @@
+#ifndef _QUOTE_
+#define _QUOTE_
+
+#include <iostream>
+#include <string>
+
+class Quote{
+    public:
+        Quote() = default;
+        Quote(const std::string &book, double sales_price):
+            bookNo(book), price(sales_price){}
+        std::string isbn() const { return bookNo; }
+        virtual double net_price(std::size_t n) const {
+            return n * price;
+        }
+
+        virtual ~Quote() = default;
+    private:
+        std::string bookNo;
+    protected:
+        double price = 0.0;
+};
+
+#endif
