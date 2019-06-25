c0e4b9dcf315c498749cc64296335f04d81d6041
yyhclimacool <yyhclimacool@gmail.com>
2019-06-25 23:39:40 +0800

    elimdups

--- Chapter10/exercise_10_09.cc
+++ Chapter10/exercise_10_09.cc
@@ -4,8 +4,27 @@
 
 using namespace std;
 
-void elimDups() {
+void elimDups(vector<string> &words) {
+    for_each(words.cbegin(), words.cend(), [] (const string &s) { cout << s << '\t'; });
+    cout << endl;
+
+    sort(words.begin(), words.end());
+
+    auto end_unique = unique(words.begin(), words.end());
+
+    for_each(words.cbegin(), words.cend(), [] (const string &s) { cout << s << '\t'; });
+    cout << endl;
+
+    words.erase(end_unique, words.end());
+
+    for_each(words.cbegin(), words.cend(), [] (const string &s) { cout << s << '\t'; });
+    cout << endl;
 }
 
 int main() {
+    vector<string> svec{"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle"};
+
+    elimDups(svec);
+
+    return 0;
 }
