23948a4b5a8659f6e869c6ab54800dab2552553d
AeroBoost <yyhclimacool@gmail.com>
2019-07-10 10:30:20 +0000

    word_count

---
+++ Chapter11/exercise_11_03.cc
@@ -0,0 +1,18 @@
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+void word_count(istream &is) {
+    map<string, size_t> word_count;
+    string word;
+    while (is >> word)
+        ++word_count[word];
+    // output
+    for (const auto &w : word_count)
+        cout << w.first << " occurs " << w.second << " times" << endl;
+}
+
+int main() {
+    word_count(cin);
+}
