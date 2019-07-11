c8d0b5b1835caa0859ef59bb3198bafebacea1fd
AeroBoost <yyhclimacool@gmail.com>
2019-07-11 03:01:48 +0000

    bidirectional iterator do not suppert < operator

--- Chapter11/exercise_11_10.cc
+++ Chapter11/exercise_11_10.cc
@@ -9,4 +9,8 @@ int main() {
     map<vector<int>::iterator, int> map1;
     // list的迭代器是双向迭代器
     map<list<int>::iterator, int> map2;
+
+    list<int> ilst = {1,2,3,4,4,5};
+    // 这里会报错，因为list的迭代器不支持<运算符
+    map2.insert(make_pair(ilst.begin(), 42));
 }
