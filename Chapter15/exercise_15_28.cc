d647665bfcdb1ce21b395353bca38e3c8107eed9
yyhclimacool <yyhclimacool@gmail.com>
2019-08-11 23:36:34 +0800

    vector contains shared_ptr of base class 'Quote'

---
+++ Chapter15/exercise_15_28.cc
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <memory>
+
+#include "exercise_15_05.h"
+
+using namespace std;
+
+int main() {
+    vector<Quote> basket;
+
+    basket.push_back(Bulk_quote("aaaa", 12.0, 10, .1));
+    basket.push_back(Bulk_quote("bbbb", 14., 15, .2));
+
+    // no discount actually
+    for (const auto &e : basket){
+        cout << e.net_price(20) << endl;
+    }
+
+    return 0;
+}
