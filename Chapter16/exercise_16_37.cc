84be6d12881673bbae4e2986b68108639c0c41cd
yyhclimacool <yyhclimacool@gmail.com>
2019-09-08 13:26:19 +0000

    explicit template argument

---
+++ Chapter16/exercise_16_37.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+  int ival = 42;
+  double dval = 42.42;
+
+  // Compile error
+  auto val = std::max(ival, dval);
+  cout << val << endl;
+}
