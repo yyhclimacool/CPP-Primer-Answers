5eb3dcffc71fd15e336de577a94e88793951741e
yyhclimacool <yyhclimacool@gmail.com>
2020-02-23 14:57:06 +0000

    update chapter 02

---
+++ Chapter02/exercise_2_25.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int *pi, i, &r = i; // pi is a pointer, i is an int and r is a ref of int
+    int i1, *ip1 = 0; // i is an int, ip is a pointer to an int
+    int *ip2, ip3; // ip is a pointer to an int, ips is an int
+}
---
+++ Chapter02/exercise_2_26.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const int buf = 0;
+    int cnt = 0;
+    const int sz = cnt;
+    // ++cnt; ++sz;
+}
