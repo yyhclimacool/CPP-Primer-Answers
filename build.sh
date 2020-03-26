96974f8192db3cc5ee37e90d77d2736cce4cc833
yyhclimacool <yyhclimacool@gmail.com>
2020-03-26 05:19:28 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-${CURRENT_DIR}/build}
+BUILD_TYPE=${BUILD_TYPE:-Debug}
+INSTALL_DIR=${INSTALL_DIR:-${BUILD_DIR}/${BUILD_TYPE}_install}
+
 if [ "$1"x = "clean"x ]
 then
-    if [ -d ./build ]
+    if [ -d ${BUILD_DIR} ]
     then
-        rm -rf ./build
-        rm -rf ./output
+        rm -rf ${BUILD_DIR}
     fi
     exit 0
 fi
 
-if [ ! -d ./build ]
+if [ ! -d ${BUILD_DIR} ]
 then
-    echo "mkdir -p build"
-    mkdir -p build
+    echo "mkdir -p ${BUILD_DIR}"
+    mkdir -p ${BUILD_DIR}
 fi
 
-cmake -DCMAKE_INSTALL_PREFIX=./output -DCMAKE_BUILD_TYPE=Debug -H. -Bbuild && \
-cmake --build build -- -j && \
-cmake --build build --target install
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -H${CURRENT_DIR} -B${BUILD_DIR} && \
+cmake --build ${BUILD_DIR} -- -j && \
+cmake --build ${BUILD_DIR} --target install
