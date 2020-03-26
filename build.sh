4ac421885d46b95058ea121982438b3c55f9f17c
yyhclimacool <yyhclimacool@gmail.com>
2020-03-26 11:35:26 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -5,19 +5,46 @@ BUILD_DIR=${BUILD_DIR:-${CURRENT_DIR}/build}
 BUILD_TYPE=${BUILD_TYPE:-Debug}
 INSTALL_DIR=${INSTALL_DIR:-${BUILD_DIR}/${BUILD_TYPE}_install}
 
+COLORBLACK="\033[30m"
+COLORRED="\033[31m"
+COLORGREEN="\033[32m"
+COLORBLUE="\033[34m"
+COLORYELLOW="\033[33m"
+COLORPURPLE="\033[35m"
+COLORSKYBLUE="\033[36m"
+COLORWHITE="\033[37m"
+COLOREND="\033[0m"
+
+function LOG() {
+  date_str=`date "+%Y-%m-%d %H:%M:%S"`
+  case "$1" in
+    "WARNING")
+      shift
+      echo -e "${COLORSKYBLUE}WARNING `date "+%Y-%m-%d %H:%M:%S"` $* $COLOREND"
+      ;;
+    "ERROR")
+      shift
+      echo -e "${COLORRED}ERROR `date "+%Y-%m-%d %H:%M:%S"` $* $COLOREND"
+      ;;
+    *)
+      echo -e "${COLORGREEN}INFO `date "+%Y-%m-%d %H:%M:%S"` $* $COLOREND"
+  esac
+}
+
 if [ "$1"x = "clean"x ]
 then
-    if [ -d ${BUILD_DIR} ]
-    then
-        rm -rf ${BUILD_DIR}
-    fi
-    exit 0
+  if [ -d ${BUILD_DIR} ]
+  then
+    LOG WARNING "rm -rf ${BUILD_DIR}"
+    rm -rf ${BUILD_DIR}
+  fi
+  exit 0
 fi
 
 if [ ! -d ${BUILD_DIR} ]
 then
-    echo "mkdir -p ${BUILD_DIR}"
-    mkdir -p ${BUILD_DIR}
+  LOG "mkdir -p ${BUILD_DIR}"
+  mkdir -p ${BUILD_DIR}
 fi
 
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -H${CURRENT_DIR} -B${BUILD_DIR} && \
