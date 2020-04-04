bc4c293412d99b80f60ec8000052b5d5c56552b4
kevin <yyhclimacool@gmail.com>
2020-04-04 18:14:37 +0800

    47

--- Chapter16/exercise_16_47.cc
+++ Chapter16/exercise_16_47.cc
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-/*
 template <typename F, typename T1, typename T2>
 void flip1(F f, T1 t1, T2 t2) {
   f(t2, t1);
@@ -13,7 +12,6 @@ template <typename F, typename T1, typename T2>
 void flip2(F f, T1 &&t1, T2 &&t2) {
   f(t2, t1);
 }
-*/
 
 template <typename F, typename T1, typename T2>
 void flip3(F f, T1 &&t1, T2 &&t2) {
@@ -33,23 +31,21 @@ void f3(int &&a, double &b) {
 int main() {
   int ival = 42;
   double dval = 3.14;
-  int &rival = ival;
-  /*
   // 调用参数类型是非引用类型的函数是可以的
   flip1(f1, 3.33, 42);
   // 没法改变dval的值
   flip1(f2, dval, 42);
   cout << "dval = " << dval << endl;
-  */
+  // 实例化报错
+  //flip1(f3, dval, 42);
   // ---------------------------------------------------------------------------------
-  /*
+  cout << "------------------------------" << endl;
   flip2(f1, 3.33, 42);
   // 通过使用类型为模板类型右值引用的参数，可以将实参的状态传递给里层函数
   flip2(f2, dval, 42);
   cout << "dval = " << dval << endl;
   // 但是flip2调用参数为右值引用的函数时，发生编译错误：即使我们传递的是一个右值
-  flip2(f3, dval, 42);
-  */
+  // flip2(f3, dval, 42);
   flip3(f1, 3.33, 42);
   flip3(f2, dval, 42);
   cout << "dval = " << dval << endl;
